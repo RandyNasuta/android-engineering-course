2536ce8451f96271054fbfdde4304030b8c6b9e0
Randy Nasuta <randynasuta123@gmail.com>
2022-04-14 12:50:16 +0700

    audio demo app

---
+++ app/src/main/java/com/example/audiodemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.audiodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void playMusic(View view){
+        mediaPlayer.start();
+    }
+
+    public void pauseMusic(View view){
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.kanatacover);
+
+    }
+}
